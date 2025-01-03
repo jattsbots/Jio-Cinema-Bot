61b66026e8d2b11874b21fc594ccfa853d1ed534
jatts <bxjjjzid@gmail.com>
2025-01-03 18:11:07 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -15,12 +15,12 @@ def get_env_variable(name: str, default=None, is_int=False):
     return value
 
 class Config:
-    APP_ID = get_env_variable("APP_ID", 1543212, is_int=True)
-    API_HASH = get_env_variable("API_HASH", "d47de4b25ddf79a08127b433de32dc84")
-    AUTH_USERS = get_env_variable("AUTH_USERS", "1478357602 5485818124 1738852527").split()
-    BOT_TOKEN = get_env_variable("BOT_TOKEN", "5502968436:AAHtTd9M1T0OlRv5akFGO3fGCwtfIs0INis")
+    APP_ID = get_env_variable("APP_ID", "26649585", is_int=True)
+    API_HASH = get_env_variable("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c)
+    AUTH_USERS = get_env_variable("AUTH_USERS", "6052303737").split()
+    BOT_TOKEN = get_env_variable("BOT_TOKEN", "7806927257:AAEwwyQ2RZVmCqsZVdIlR_znhcNa5ppZQ8k")
     BOT_START_TIME = time()
-    DB_URI = get_env_variable("DATABASE_URL", "mongodb+srv://gtm:gtm@cluster0.mc904kw.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = get_env_variable("DATABASE_URL", "mongodb+srv://anmolrender1:singh@cluster0.sizc8ea.mongodb.net/")
     DEFAULT_THUMB = get_env_variable("DEFAULT_THUMB", "https://placehold.it/90x90")
     REQUEST_DELAY = get_env_variable("REQUEST_DELAY", 120, is_int=True)
     PARENT_ID = get_env_variable("PARENT_ID", "1CI7KC0YrbPzd24XhySb9lVZRCXdg-sRG")
@@ -30,7 +30,7 @@ class Config:
     FORCE_SUB_CHANNEL = get_env_variable('FORCE_SUB_CHANNEL', 'false')
     FORCE_SUB_CHANNEL = FORCE_SUB_CHANNEL.lower() == 'true' if FORCE_SUB_CHANNEL else False
 
-    INDEX_URL = get_env_variable('INDEX_URL', 'https://red-violet-8263.thefile2linkrobot.workers.dev/0')
+    INDEX_URL = get_env_variable('INDEX_URL', 'https://pbx1rencoder.anmxl0008.workers.dev')
     INDEX_URL = None if len(INDEX_URL) == 0 else INDEX_URL
 
     IS_TEAM_DRIVE = get_env_variable('IS_TEAM_DRIVE', 'false')
